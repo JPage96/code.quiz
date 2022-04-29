79e7eed7dcfd8f4bbd2137529f0366f2bab22f75
JPage96 <justice.page1@pepsico.com>
2022-04-28 22:27:22 -0500

    added highschool file and questions

---
+++ assets/java/questions.js
@@ -0,0 +1,54 @@
+var questions = [
+    {
+      title: "Which character is used to indicate an end tag?",
+      choices: ["^", "/", "*", "<"],
+      answer: "/"
+    },
+    {
+      title: "What does CSS stand for?.",
+      choices: ["Computer Style Sheets", "Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets"],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      title: "How do you create a function in JavaScript?",
+      choices: ["function = myFunction()", "function myFunction()", "function:myFunction()", "all of the above"],
+      answer: "function myFunction()"
+    },
+    {
+      title:"The Bootstrap grid system is based on how many columns?",
+      choices: ["12", "6", "9", "3"],
+      answer: "12"
+    },
+    {
+      title: "Which sign does jQuery use as a shortcut for jQuery?",
+      choices: ["The $ sign", "The ? sign", "The % sign", "The # sign"],
+      answer: "The $ sign"
+    }, 
+    {
+      title: "HTML uses ____?",
+      choices: ["User-defined tags", "Predefined tags", "Fixed tags defined by the language", "Tags for links only"],
+      answer: "Fixed tags defined by the language"
+    },
+    {
+      title: "Which of tyhe following is not a browser?",
+      choices: ["Mozilla Firefox", "Netscape", "Microsoft Bing", "Opera"],
+      answer: "Microsoft Bing"
+    },
+    {
+      title: "If you want to set a style for just one element, what selector will you use?",
+      choices: ["id", "Text", "class", "name"],
+      answer: "id"
+    },
+    {
+      title: "What is the alternative to the <b> tag to make text bold?",
+      choices: ["<bold>", "<strong>", "<i>", "<emp>"],
+      answer: "<strong>"
+    },
+    {
+      title: "Choose the correct HTMl tag to render text in italics?",
+      choices: ["<itialic>", "<il>", "<it", "<i>"],
+      answer: "<i>"
+    }
+
+];
+    
